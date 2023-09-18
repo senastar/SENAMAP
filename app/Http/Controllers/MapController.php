73ed23e14742e7cebeba39540e89408a577ab8cb
senastar <senamario@icloud.com>
2023-09-18 23:43:48 +0900

    使い方サイトの作成、フレームワークの作成

--- app/Http/Controllers/MapController.php
+++ app/Http/Controllers/MapController.php
@@ -3,5 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Map;
 
+
+class MapController extends Controller
+{
+public function index(){
+    return view ('index');}
+    
+public function howuse(){
+    return view ('use');
 }
+}
+
+
